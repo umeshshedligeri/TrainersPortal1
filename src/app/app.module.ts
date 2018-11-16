b9e0350f7c436d70f3954626ded4907e138c8963
vinodbiradar <vinodbirada555@gmail.com>
2018-11-16 10:52:25 +0530

    took values to service and validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule ,NO_ERRORS_SCHEMA } from '@angular/core';
 import { NavbarModule, WavesModule, ButtonsModule ,ModalModule,InputsModule} from 'angular-bootstrap-md';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NgFlashMessagesModule } from 'ng-flash-messages';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
 
 
 
@@ -9,6 +12,7 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import {AuthService} from './services/auth.service'
 
 
 @NgModule({
@@ -24,9 +28,12 @@ import { NavbarComponent } from './navbar/navbar.component';
     ButtonsModule,
     MDBBootstrapModule,
     ModalModule.forRoot(),
-    InputsModule
+    InputsModule,
+    NgFlashMessagesModule,
+    HttpClientModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent],
   schemas: [ NO_ERRORS_SCHEMA ]
 })
--- src/app/trainers/trainers.module.ts
+++ src/app/trainers/trainers.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { FormsModule,ReactiveFormsModule} from '@angular/forms'
 import { NavbarModule, WavesModule, ButtonsModule ,ModalModule,InputsModule} from 'angular-bootstrap-md';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NgFlashMessagesModule } from 'ng-flash-messages';
+import {HttpClientModule} from '@angular/common/http';
+import {HttpModule} from '@angular/http';
+
 
 import { TrainersRoutingModule } from './trainers-routing.module';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import {ValidateService} from '../services/validate.service';
+import {AuthService} from '../services/auth.service';
 
 @NgModule({
   imports: [
@@ -19,9 +25,14 @@ import { RegisterComponent } from './register/register.component';
     MDBBootstrapModule,
     ModalModule,
     InputsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgFlashMessagesModule,
+    HttpClientModule,
+    HttpModule
+    
   ],
   declarations: [LoginComponent, HomeComponent, RegisterComponent],
-  exports:[LoginComponent]
+  exports:[LoginComponent],
+  providers:[ValidateService,AuthService]
 })
 export class TrainersModule { }
